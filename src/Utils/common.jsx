bb12083b9bfc368d4a79767a8b9528b70509c07e
ADARSH RAJ <90273605+adarsh02raj@users.noreply.github.com>
2024-01-09 23:23:08 +0530

    Update common.jsx

--- src/Utils/common.jsx
+++ src/Utils/common.jsx
@@ -1,1 +1,3 @@
-export const BASE_URL = "https://shopperassistant.anncode.com/admin/collection"
+// export const BASE_URL = "https://shopperassistant.anncode.com/admin/collection"
+
+export const BASE_URL = "https://corsproxy.org/?https://shopperassistant.anncode.com/admin/collection"
